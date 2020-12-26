df72d336373f3bc74d17e02b7670f351d0cdff12
Sakura-del <2653433352@qq.com>
2020-12-26 10:17:10 +0800

    v1.2.4

--- service/service_user/src/main/java/com/lgc/user_service/config/Config.java
+++ service/service_user/src/main/java/com/lgc/user_service/config/Config.java
@@ -6,10 +6,12 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @MapperScan("com.lgc.user_service.mapper")
 //这是配置类哦
+@EnableAsync
 public class Config {
     @Bean
     public ISqlInjector sqlInjector(){
@@ -20,5 +22,14 @@ public class Config {
     public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
         return paginationInterceptor;
+    }
+    @Bean
+    public ThreadPoolTaskExecutor getExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.initialize();
+        return executor;
     }
 }
