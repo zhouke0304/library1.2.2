42d63c6a21b7f7902963345b1ddc370faa17e6b8
Sakura-del <2653433352@qq.com>
2020-12-26 10:18:25 +0800

    v1.2.4

--- service/service_user/src/main/java/com/lgc/user_service/config/Config.java
+++ service/service_user/src/main/java/com/lgc/user_service/config/Config.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
